b01aa26802f9f15af17a3650f442b1764f2ce418
MauricioL88 <lisboa.sse@gmail.com>
2024-02-09 17:58:41 -0300

    alterando o escopo do projeto

--- 00 - testes/teste1.py
+++ 00 - testes/teste1.py
@@ -1,27 +1,49 @@
-conta = 0
-banco_dados = {}
-saldo = 0.0
+import textwrap
 
-resp = 'N'
-resp.lower()
-while resp != 's':
-    conta = len(banco_dados)
-    nome = input("Digite o seu nome: ")
-    cpf = int(input("Digite o seu CPF: "))
+def menu():
+    menu = '''\n
+    =============== MENU ===============
+    [d]\tDepositar
+    [s]\tSacar
+    [e]\tExtrato
+    [nc]\tNova Conta
+    [lc]\tListar Contas
+    [nu]\tNovo Usuário
+    [q]\tSair
+    => : '''
+    return input(textwrap.dedent(menu))
 
-    banco_dados.update({
-        conta: {
-            'nome': nome,
-            'cpf': cpf,
-            'saldo': saldo
-        }
-    })
 
-    print("\nTamanho do Banco de Dados: ", len(banco_dados))
-    resp = input("\nDeseja sair?\nS | N: ")
+def tela_inicial():
+    conta = 0
+    banco_dados = {}
+    saldo = 0.0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+
+    resp = 'N'
     resp.lower()
-    print("\n")
+    while resp != 's':
+        opcao = menu()
 
-for chave, valor in banco_dados.items():
-    print(chave, valor)
-print("\n")
+        conta = len(banco_dados)
+        nome = input("Digite o seu nome: ")
+        cpf = int(input("Digite o seu CPF: "))
+
+        banco_dados.update({
+            conta: {
+                'nome': nome,
+                'cpf': cpf,
+                'saldo': saldo
+            }
+        })
+
+        print("\nTamanho do Banco de Dados: ", len(banco_dados))
+        resp = input("\nDeseja sair?\nS | N: ")
+        resp.lower()
+        print("\n")
+
+    for chave, valor in banco_dados.items():
+        print(chave, valor)
+    print("\n")
