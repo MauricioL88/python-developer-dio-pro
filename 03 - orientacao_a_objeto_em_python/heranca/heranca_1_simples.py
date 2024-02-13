a46da8efa6d3e5fe7b12d06660f27978745cf2c7
MauricioL88 <lisboa.sse@gmail.com>
2024-02-13 20:26:13 -0300

    Trabalhando com herança simples

--- 03 - orientacao_a_objeto_em_python/heranca/heranca_1_simples.py
+++ 03 - orientacao_a_objeto_em_python/heranca/heranca_1_simples.py
@@ -1,2 +1,46 @@
 class Veiculo:
+    # Atributos em comum
+    def __init__(self, cor, placa, num_rodas):
+        self.cor = cor
+        self.placa = placa
+        self.num_rodas = num_rodas
+
+    # Comportamentos em comum
+    def ligar_motor(self):
+        print("Ligando motor...")
+
+    # Método "__str__" exibe os dados do objeto
+    def __str__(self):
+        return f"{self.__class__.__name__} - {' | '.join([f'{chave}: {valor}' for chave, valor in self.__dict__.items()])}"
+
+
+class Motocicleta(Veiculo):
     pass
+
+
+class Carro(Veiculo):
+    pass
+
+
+class Caminhao(Veiculo):
+    # Sobrescrevendo o método contrutor "pai", personalizando para a classe "filha"
+    def __init__(self, cor, placa, num_rodas, carregado):
+        super().__init__(cor, placa, num_rodas)
+        self.carregado = carregado
+
+    # Comportamento específico de uma classe filha
+    def esta_carregado(self):
+        print(f"{"Sim, eu" if self.carregado else "Nao"} estou carregado...")
+
+
+moto = Motocicleta("Verde", "AAA0000", 2)
+moto.ligar_motor()
+
+carro = Carro("Preto", "AAA0001", 4)
+carro.ligar_motor()
+
+caminhao = Caminhao("Azul", "AAA0002", 8, False)
+caminhao.ligar_motor()
+
+print(caminhao)
+caminhao.esta_carregado()
