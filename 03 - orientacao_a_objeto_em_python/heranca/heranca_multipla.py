3f800e7db0ae85a0c730a1a4bc793634269dc370
MauricioL88 <lisboa.sse@gmail.com>
2024-02-13 09:41:05 -0300

    Estundando Herança em POO

---
+++ 03 - orientacao_a_objeto_em_python/heranca/heranca_multipla.py
@@ -0,0 +1,13 @@
+# Quando uma classe herda de várias classes "pai";
+# Não são todas as linguegens de programação que suportam esse recurso, ou considera uma má prática;
+
+# SINTÁXE
+class A:
+    pass
+
+
+class B:
+    pass
+
+class C(A, B): # Classe "C" extende das classes "A" e "B"
+    pass 
---
+++ 03 - orientacao_a_objeto_em_python/heranca/sintaxe_heranca.py
@@ -0,0 +1,8 @@
+# Sintaxe básica de uma herança
+
+class A:
+    pass
+
+# Exemplo de herança simples: quando uma classe herda somente de uma classe "pai".
+class B(A): # Classe B herda (extende) da classe "A".
+    pass
